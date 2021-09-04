6c84debf10d17a0f92a35d042786a09543d53376
srushti8 <76839929+srushti8@users.noreply.github.com>
2021-09-04 16:03:17 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -6,6 +6,7 @@ const Body = Matter.Body;
 
 var roof, ground, rope1;
 var bob1, bob2, bob3, bob4, bob5;
+var startPosx, startPosy, bobD;
 
 function preload()
 {
@@ -13,19 +14,22 @@ function preload()
 }
 
 function setup() {
-	createCanvas(800, 700);
+	createCanvas(1600, 700);
 
 
 	engine = Engine.create();
 	world = engine.world;
 
-	roof = new Roof(400,200,600,40);
-	ground = new Roof(400,700,800,10);
-	bob1 = new Bob(200,300,100);
-	bob2 = new Bob(300,300,100);
-	bob3 = new Bob(400,300,100);
-	bob4 = new Bob(500,300,100);
-	bob5 = new Bob(600,300,100);
+	roof = new Roof(width/2,height/4,width/7,20);
+	//ground = new Roof(400,700,800,10);
+	startPosx = width/2;
+	startPosy = height/4+500;
+	bobD = 40;
+	bob1 = new Bob(startPosx-bobD*2,startPosy,bobD);
+	bob2 = new Bob(startPosx-bobD,startPosy,bobD);
+	bob3 = new Bob(startPosx,startPosy,bobD);
+	bob4 = new Bob(startPosx+bobD,startPosy,bobD);
+	bob5 = new Bob(startPosx+bobD*2,startPosy,bobD);
 
 	rope1 = new Rope(bob1.body,roof.body,-100*2,0);
 	rope2 = new Rope(bob2.body,roof.body,-50*2,0);
@@ -39,7 +43,7 @@ function draw() {
   background(172,244,247);
   
   roof.display();
-  ground.display();
+  //ground.display();
   bob1.display();
   bob2.display();
   bob3.display();

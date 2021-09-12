39d1697a637d5bd0ab7008427031a4742de149eb
srushti8 <76839929+srushti8@users.noreply.github.com>
2021-09-12 18:04:49 +0530

    Add files via upload

--- Bob.js
+++ Bob.js
@@ -1,23 +1,24 @@
 class Bob {
   constructor(x,y,r) {
-  var options={ 
-  isStatic:false, 
-  restitution:0.3, 
-  friction:0, 
-  density:1.2 
-  } 
-  this.x=x; 
-  this.y=y; 
-  this.r=r 
-  this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options) 
-  World.add(world, this.body); 
+    var options={ 
+      isStatic:false, 
+      restitution:0.4, 
+      friction:0, 
+      density:0.8
+    } 
+    this.x=x; 
+    this.y=y; 
+    this.r=r 
+    this.body=Bodies.circle(x, y, r/2, options);
+    World.add(world, this.body); 
   } 
   display() {
-  var paperpos=this.body.position; 
-  push(); 
-  translate(paperpos.x, paperpos.y); 
-  fill(255,60,117); 
-  ellipse(0,0,this.r, this.r);
-  pop(); 
+    var paperpos=this.body.position; 
+    push(); 
+    translate(this.x, this.y);
+    rectMode(CENTER) 
+    fill(255,60,117); 
+    ellipse(0,0,this.r, this.r);
+    pop(); 
   } 
 }
